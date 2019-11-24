a58ca1fb9e6253abdcd82447e6b10d8e332c9dd8
rebecca-millerr <52360419+rebecca-millerr@users.noreply.github.com>
2019-11-24 10:41:45 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,21 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 5000;                   // runs on localhost:5000
+const models = require('./models');  // see models/index.js and other files
+const routes = require('./routes');  // see routes
+let db;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-  res.send('Home');
-});
+app.use('/api', routes);
 
-app.get('/meals', function (req, res) {
-  res.send('Meals');
-});
+const startDB = (done) => {
+  models.init((database) => {
+    routes.init(database);
+    db = database;
+    db.sequelize.sync({ force: true }).then(() => {   // force true means the database clears every time it starts
+      done();
+    });
+  }
+}
+              
+// start app or defer to test env and provide utils
+if (process.env.NODE_ENV !== 'test') {
+  startDB(() => {
+    app.listen(port, () => {
+      console.log(`App running on port ${port}.`);
+    });
+  });
+} else {
+  app.startDB = startDB
+  module.exports = app;
+}
 
+// app.get('/', function (req, res) {
+//   res.send('Home');
+// });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
-});
+// app.get('/meals', function (req, res) {
+//   res.send('Meals');
+// });
