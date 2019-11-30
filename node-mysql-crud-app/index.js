8bf3cb8562bd352e6768d798299f85697f91a066
dcolak01 <54953926+dcolak01@users.noreply.github.com>
2019-11-30 00:00:08 -0500

    ok this is much better

--- node-mysql-crud-app/index.js
+++ node-mysql-crud-app/index.js
@@ -1,29 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const path = require('path');
 const app = express();
-
-const {getHomePage} = require('./routes/index');
-const {addUserPage, addUser, deleteUser, editUser, editUserPage} = require('./routes/user');
-const port = 3306;
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'InShape_20',
+    database: 'inshape'
 });
 
+const {getHomePage} = require('./routes/index.js');
+const {addUserPage, addUser, deleteUser, editUser, editUserPage} = require('./routes/user.js');
+const port = 5000;
+
 // Connect
-db.connect((err) => {
-    if(err){
-        //throw err;
-        console.log("yarrak");
-    }
+db.connect(function(err) {
+    if(err) throw err;
     console.log('MySql Connected...');
 });
 
-// // Create DB
+// Create DB
 // app.get('/createdb', (req, res) => {
 //     let sql = 'CREATE DATABASE inshape';
 //     db.query(sql, (err, result) => {
@@ -33,9 +33,9 @@ db.connect((err) => {
 //     });
 // });
 
-// // Create table
+// // // Create table
 // app.get('/createpoststable', (req, res) => {
-//     let sql = 's CREATE TABLE users (id int(5) NOT NULL AUTO_INCREMENT, first_name varchar(255) NOT NULL, last_name varchar(255) NOT NULL, email varchar(255) NOT NULL, password varchar(255) NOT NULL, birth date NOT NULL, sex char(1) NOT NULL, height int(11) NOT NULL, weight int(11) NOT NULL, bmi float NOT NULL, bmi_goal float NOT NULL, created date NOT NULL, modified date NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin';
+//     let sql = 'CREATE TABLE inshape.users (id int(5) NOT NULL AUTO_INCREMENT, first_name varchar(255) NOT NULL, last_name varchar(255) NOT NULL, email varchar(255) NOT NULL, password varchar(255) NOT NULL, birth date NOT NULL, sex char(1) NOT NULL, height int(11) NOT NULL, weight int(11) NOT NULL, bmi float NOT NULL, bmi_goal float NOT NULL, created date NOT NULL, modified date NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=latin1';
 //     db.query(sql, (err, result) => {
 //         if(err) throw err;
 //         console.log(result);
@@ -43,25 +43,24 @@ db.connect((err) => {
 //     });
 // });
 
-// // configure middleware
-// app.set('port', process.env.port || port); // set express to use this port
-// app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-// app.set('view engine', 'ejs'); // configure template engine
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json()); // parse form data client
-// app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
-// app.use(fileUpload()); // configure fileupload
+// configure middleware
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
 
 // // routes for the app
-// app.get('/', getHomePage);
-// app.get('/add', addPlayerPage);
-// app.get('/edit/:id', editPlayerPage);
-// app.get('/delete/:id', deletePlayer);
-// app.post('/add', addPlayer);
-// app.post('/edit/:id', editPlayer);
+app.get('/', getHomePage);
+app.get('/add', addUserPage);
+app.get('/edit/:id', editUserPage);
+app.get('/delete/:id', deleteUser);
+app.post('/add', addUser);
+app.post('/edit/:id', editUser);
 
 
-// set the app to listen on the port
+// // set the app to listen on the port
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
